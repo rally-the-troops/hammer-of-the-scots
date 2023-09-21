254265467731865a1697f5aa6e91def55ff3dd08
Tor Andersson <tor@ccxvii.net>
2023-09-21 21:06:20 +0200

    Add match making setups.

--- title.sql
+++ title.sql
@@ -1,1 +1,5 @@
 insert or replace into titles ( title_id, title_name, bgg ) values ( 'hammer-of-the-scots', 'Hammer of the Scots', 3685 );
+insert or ignore into setups ( title_id, player_count, scenario, options ) values
+	( 'hammer-of-the-scots', 2, 'Braveheart', '{"delay_hits":true}' ),
+	( 'hammer-of-the-scots', 2, 'The Bruce', '{"delay_hits":true}' )
+;
